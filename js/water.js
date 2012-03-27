64ce337df76bbc9b8f274b6f70bc6171847fe818
Lars Grammel <lars.grammel@gmail.com>
2012-03-27 12:51:53 -0700

    removed number slider

--- js/water.js
+++ js/water.js
@@ -87,68 +87,6 @@ function setLocalStorageValue(key, value) {
 	localStorage.setItem([localStorageKey, key].join('/'), value);
 }
 
-// if we click on a numeric constant, select the token and show the slider
-var chosenRow, chosenColumn;
-var onNumeric = false;
-window.aceEditor.on("click", function(e) {
-
-	var editor = e.editor;
-	var pos = editor.getCursorPosition();
-	var token = editor.session.getTokenAt(pos.row, pos.column);
-	onNumeric = false;
-
-	// did we click on a number?
-	if (token && /\bconstant.numeric\b/.test(token.type)) {
-
-		// stop pulsing numerics
-		if (pulseNumerics) {
-			window.clearInterval(pulse);
-			pulseNumerics = false;
-		}
-
-		// set the slider params based on the token's numeric value
-		// TODO: there has to be a better way of setting this up
-		// TODO: feels pretty silly at the moment
-		if (token.value == 0) {
-			var sliderRange = [-100, 100];
-		} else {
-			var sliderRange = [-token.value * 3, token.value * 5];
-		}
-		slider.slider('option', 'max', d3.max(sliderRange));
-		slider.slider('option', 'min', d3.min(sliderRange));
-
-		// slider range needs to be evenly divisible by the step
-		if ((d3.max(sliderRange) - d3.min(sliderRange)) > 20) {
-			slider.slider('option', 'step', 1);
-		} else {
-			slider.slider('option', 'step', (d3.max(sliderRange) - d3.min(sliderRange))/200);
-		}
-		slider.slider('option', 'value', token.value);
-
-		// position slider centered above the cursor
-		var scrollerOffset = $('.ace_scroller').offset();
-		var cursorOffset = editor.renderer.$cursorLayer.pixelPos;
-		var sliderTop = scrollerOffset.top + cursorOffset.top - Number($('#editor').css('font-size').replace('px', ''))*0.8;
-		var sliderLeft = scrollerOffset.left + cursorOffset.left - slider.width()/2;
-
-		// sync the slider size with the editor size
-		slider.css('font-size', $('#editor').css('font-size'));
-		slider.css('font-size', '-=4');
-		slider.offset({top: sliderTop, left: sliderLeft});
-
-		// allow the slider to be shown
-		onNumeric = true;
-
-		// make this position globally scoped
-		chosenRow = pos.row;
-		chosenColumn = token.start;
-
-		// prevent click event from bubbling up to body, which
-		// would then trigger an event to hide the slider
-		e.stopPropagation();
-	}
-});
-
 // turn off horizontal scrollbar
 window.aceEditor.renderer.setHScrollBarAlwaysVisible(false);
 
@@ -186,50 +124,6 @@ window.aceEditor.replace = function(replacement) {
 			this.selection.setSelectionRange(range);
 	}
 }
-
-// create slider
-var slider = $('#slider');
-slider.slider({
-	slide: function(event, ui) {
-
-		// set the cursor to desired location
-		var cursorPosition = window.aceEditor.getCursorPosition();
-		if (!(cursorPosition.row == chosenRow && cursorPosition.column == chosenColumn)) {
-			window.aceEditor.getSelection().moveCursorTo(chosenRow, chosenColumn);
-
-			// clear selection
-			window.aceEditor.clearSelection();
-		}
-
-		// get token
-		var token = window.aceEditor.session.getTokenAt(chosenRow, chosenColumn + 1);
-
-		// find and replace
-		window.aceEditor.find(String(token.value));
-		window.aceEditor.replace(String(ui.value));
-	}
-});
-
-// use control key on linux, alt key everywhere else
-var sliderKey = navigator && navigator.platform && navigator.platform.toLowerCase().indexOf('linux') != -1
-	? 'ctrl' : 'alt';
-
-// display slider key on page
-$('#sliderKey').text(sliderKey);
-
-// trigger slider on control
-$('textarea').bind('keydown.' + sliderKey, function(e) {
-	// are we on a token?
-	if (onNumeric) {
-		slider.css('visibility', 'visible'); 
-	}
-}).bind('keyup.' + sliderKey, function(e) {
-	slider.css('visibility', 'hidden');
-});
-
-$('#slider').bind('keyup.' + sliderKey, function(e) {
-	slider.css('visibility', 'hidden');
-});
 
 // we're not a numeric, by default
 // if we are, the editor click will handle it
