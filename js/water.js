1fb70d04ebe8e3a13bee1876acef62790c9302b1
Wlodek Bzyl <matwb@ug.edu.pl>
2012-03-28 23:49:55 +0200

    black -> white

--- js/water.js
+++ js/water.js
@@ -16,7 +16,8 @@ setSvgDimensions();
 window.aceEditor = ace.edit("editor");
 
 // set the theme
-window.aceEditor.setTheme("ace/theme/twilight");
+// window.aceEditor.setTheme("ace/theme/twilight");
+window.aceEditor.setTheme("ace/theme/crimson_editor");
 
 // set mode to javascript
 var JavaScriptMode = require("ace/mode/javascript").Mode;
