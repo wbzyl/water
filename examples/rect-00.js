d31bf64d5968d4500733ca3d1c18afa1604912d9
Wlodek Bzyl <matwb@ug.edu.pl>
2012-03-29 12:00:27 +0200

    poprawki w rect-00

--- examples/rect-00.js
+++ examples/rect-00.js
@@ -8,12 +8,12 @@ var svg = d3.select("body")
 // Dodajemy elementy SVG do strony
 
 svg.append("rect")
-     .attr("x", 100)
-     .attr("y", 50)
-     .attr("width", 400)
-     .attr("height", 300)
-     .attr("fill-opacity", 0.5)
-     .attr("stroke-opacity", 0.5);
+  .attr("x", 100)
+  .attr("y", 50)
+  .attr("width", 400)
+  .attr("height", 300)
+  .attr("fill-opacity", 0.5)
+  .attr("stroke-opacity", 0.5);
 
 // Stylizacja (jQuery)
 
@@ -22,6 +22,7 @@ $('circle, rect')
   .css('fill-opacity', 0.6)
   .css('stroke', '#222')
   .css('stroke-width', 20)
-  .on("mouseover", function() {d3.select(this).style("fill", "#00a");})
-  .on("mouseout", function() {d3.select(this).style("fill", "#000");});
+  .css("stroke-dasharray", [15, 10, 5, 10, 15])
+  .on("mouseover", function() { d3.select(this).style("fill", "#00a"); })
+  .on("mouseout", function() { d3.select(this).style("fill", "#000"); });
 
