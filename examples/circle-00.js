38da5c1178c7187f6f6e4dfcb1f7aee5e7c212ca
Wlodek Bzyl <matwb@ug.edu.pl>
2012-03-29 11:07:46 +0200

    more '00' examples: line, rect, circle

--- examples/circle-00.js
+++ examples/circle-00.js
@@ -2,18 +2,24 @@
 
 var svg = d3.select("body")
   .append("svg")
-    .attr("width", 510)
-    .attr("height", 360);
+    .attr("width", 610)
+    .attr("height", 400);
 
 // Dodajemy elementy SVG do strony
 
+svg.append("circle")
+  .attr("r",  150)
+  .attr("cx", 300)
+  .attr("cy", 200)
+  .attr("stroke-opacity", 0.2);
+
 svg.append("rect")
-     .attr("x", 100)
-     .attr("y", 50)
-     .attr("width", 400)
-     .attr("height", 300)
-     .attr("fill-opacity", 0.5)
-     .attr("stroke-opacity", 0.5);
+  .attr("x", 500)
+  .attr("y", 50)
+  .attr("width", 100)
+  .attr("height", 300)
+  .attr("fill-opacity", 0.5)
+  .attr("stroke-opacity", 0.5);
 
 // Stylizacja (jQuery)
 
@@ -24,4 +30,3 @@ $('circle, rect')
   .css('stroke-width', 20)
   .on("mouseover", function() {d3.select(this).style("fill", "#00a");})
   .on("mouseout", function() {d3.select(this).style("fill", "#000");});
-
